df82522b384c0c74675b6e84acf2d39fd7ad92db
rin-11 <erinkates11@gmail.com>
2024-10-29 20:17:22 -0400

    setup

---
+++ mini-proj-1.py
@@ -0,0 +1,9 @@
+# Write a code to simulate dice-rolling. Since rolling of a dice is a random event and can result
+# in any values from 𝑥 = 1 to 𝑥 = 6, you may want to use the random module in your code.
+# Include in your code the number of times a particular face (e.g., 𝑥 = 1) will appear when the
+# dice is rolled 𝑛 ∈ {10, 100, 10000} times. Write a function to calculate the probability that the
+# observed face is 𝑥 ∈ {1, 3, 6}. Output the probability values as the increasing of the sample size
+# 𝑛 ∈ {10, 100, 10000}. Fill the following table with your results.
+
+import random
+
